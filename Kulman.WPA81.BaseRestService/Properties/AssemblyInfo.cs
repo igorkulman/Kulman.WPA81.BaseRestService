b64fd6a6091ee5fa256f15e3297903d0c5588d06
Igor Kulman <igor@kulman.sk>
2014-12-08 13:11:23 +0100

    * new nuget package build to enable stepping through the source code

--- Kulman.WPA81.BaseRestService/Properties/AssemblyInfo.cs
+++ Kulman.WPA81.BaseRestService/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kulman.WPA81.BaseRestService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base class for a Windows Phone 8.1 XAML and Windows 8.1 REST service implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Igor Kulman")]
 [assembly: AssemblyProduct("Kulman.WPA81.BaseRestService")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.7")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
